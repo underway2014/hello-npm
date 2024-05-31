61aa3faa5edeaa1084b8a5bd7b4026d72581a802
viki <viki@qq.com>
2024-05-31 16:56:14 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import * as Big from  'big.js'
 
-function add(x: number, y: number): number{
+export const add = function(x: number, y: number): number{
     return Big(x).add(y).toNumber()
 }
-
-export default add
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import add from '../src/index'
+import {add} from '../src/index'
 
 describe("Index Tests", () => {
       it("should return 5 when 2 is added to 3", () => {
